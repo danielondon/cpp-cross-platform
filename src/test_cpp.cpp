c3b31a6c46cd5ef06ce9c8dc4a9e330a6ec8cb2f
Daniel Linux <daniel.londono@elektrobit.com>
2016-11-10 14:34:11 +0100

    Building app in Clang 3.8 in Linux

--- src/test_cpp.cpp
+++ src/test_cpp.cpp
@@ -9,7 +9,7 @@ int main()
 	cout<<"CPP Cross Platform with C++ 11 Features "<<endl;
 
 	printf("Hello Clang\n");
-	vector<int> vect{ 1, 2, 3, 4, 5 };
+	vector<int> vect {1, 3, 2, 4, 5 };
 	for (auto& el : vect)
 		cout << " - " << el << endl;
 
