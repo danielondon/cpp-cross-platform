ad527cf21bf49df33da6bfcb3d0d55361c2df13b
Londono Daniel <daniel.londono@elektrobit.com>
2018-08-09 14:40:18 +0200

    Adding coroutines project

---
+++ src/coroutines/coroutines.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <typeinfo>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    cout << "== Co-routines =====" << endl;
+
+	return 0;
+}
