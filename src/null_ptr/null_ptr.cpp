85ccdc86b1fba11052480ad722fdaa1b7a05f7de
Londono Daniel <daniel.londono@elektrobit.com>
2018-08-09 11:04:35 +0200

    Making more verbose the test of null pointers.

--- src/null_ptr/null_ptr.cpp
+++ src/null_ptr/null_ptr.cpp
@@ -10,13 +10,14 @@ public:
 	int value;
 };
 
-int f1(std::shared_ptr<Widget> spw) { cout << "f1: " << endl; return 0; }  // call these only when
-double f2(std::unique_ptr<Widget> upw) { cout << "f2: " << endl; return 1; }  // the appropriate
-float f3(Widget* pw) { cout << "f3: " << endl; return 3.0f; }              // mutex is locked
+int f1(std::shared_ptr<Widget> spw) { cout << "f1: shared PTR " << endl; return 0; }  // call these only when
+double f2(std::unique_ptr<Widget> upw) { cout << "f2: unique PTR" << endl; return 1; }  // the appropriate
+float f3(Widget* pw) { cout << "f3: raw PTR" << endl; return 3.0f; }              // mutex is locked
 
 //void overloadedFunc(std::shared_ptr<Widget> sp) { cout << "overloaded with shared pointer" << endl; }
 void overloadedFunc(std::unique_ptr<Widget> up) { cout << "overloaded with unique pointer" << endl; }
-//void overloadedFunc(Widget * rp) { cout << "overloaded with raw pointer" << endl; }
+void overloadedFunc(Widget * rp) { cout << "overloaded with raw pointer" << endl; }
+// If you comment in the void* overload, it will not build because ambiguity.
 //void overloadedFunc(void * vp) { cout << "overloaded with void pointer" << endl; }
 void overloadedFunc(int pi) { cout << "overloaded with int" << endl; }
 
@@ -31,20 +32,22 @@ int main()
 	std::cout << "nullptr -------" << std::endl;
 
 
+    std::cout << "--- Overloading of Pointers ---" << std::endl;
     { // Overload Test
         overloadedFunc(0);
-        overloadedFunc(NULL);
+        overloadedFunc(NULL); // This is a flaw
         overloadedFunc(nullptr);
     }
 
+    std::cout << "--- Test of Functions and wrappers ---" << std::endl;
 	{	
-		f1(0);
-		f2(0);
-		f3(0);
+		f1(0); // this is a flaw
+		f2(0);  // this is a flaw
+		f3(0);  // this is a flaw
 
-		f1(NULL);
-		f2(NULL);
-		f3(NULL);
+		f1(NULL);  // this is PARTIALLYa flaw. What is NULL really?
+		f2(NULL); // this is PARTIALLY a flaw. What is NULL really?
+		f3(NULL); // this is PARTIALLYa flaw. What is NULL really?
 
 		f1(nullptr);
 		f2(nullptr);
@@ -53,6 +56,7 @@ int main()
 	cout << endl << "----Using Wrappers-----" << endl;
 
 	{
+        // We only can call with pointer types
 		auto returnValue1 = wrapperFunction(f1, nullptr);
 		auto returnValue2 = wrapperFunction(f2, nullptr);
 		auto returnValue3 = wrapperFunction(f3, nullptr);
@@ -60,7 +64,7 @@ int main()
 	// This does not build, because template can not resolve 0 as a pointer
 	{
 		//auto returnValue1 = wrapperFunction(f1, 0);
-		//auto returnValue2 = wrapperFunction(f2, NULL);
+		// auto returnValue2 = wrapperFunction(f2, NULL);
 		//auto returnValue3 = wrapperFunction(f3, 0);
 	}
 
